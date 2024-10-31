655e22aa1766728686e1c32f52c16d6d77d940f7
ghiraghira <guzmanorellanap@gmail.com>
2024-10-31 20:29:24 -0300

    css progress ehen ehen

--- src/Components/Player.tsx
+++ src/Components/Player.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { songs } from './Songs';
 import './Player.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlay, faBackwardStep, faForwardStep, faPause, faShuffle, faRepeat, faArrowRight, faCircleStop } from "@fortawesome/free-solid-svg-icons";
+import { faPlay, faBackwardStep, faForwardStep, faPause, faShuffle, faRepeat, faArrowRight, faCircleStop, faVolumeHigh } from "@fortawesome/free-solid-svg-icons";
 
 const Player: React.FC = () => {
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
@@ -126,13 +126,13 @@ const Player: React.FC = () => {
   return (
     <div className="wholePlayer">
       <div className="interface">
-      <p className="playerTitle">Music Player</p>
+        <p className="songInfo">Now Playing: {songs[currentSong].title}</p>
       <img src={songs[currentSong].cover} alt={songs[currentSong].title} className="coverImg"/>
       <audio ref={audioRef} />
       <div className="playerSliders">
         <input type="range" min={0} max={songLength} step={0.1} value={currentStamp} onChange={handleSearch} className="songBar" />
       </div>
-      <p className="songInfo">Now Playing: {songs[currentSong].title}</p>
+      
       <div className="playerButtons">
         <button onClick={prevTrack} disabled={loading} className="nextButton"><FontAwesomeIcon icon={faBackwardStep} /></button>
         <button onClick={togglePlay} className="playButton">
@@ -148,14 +148,14 @@ const Player: React.FC = () => {
         </button>
       </div>
       <div className="playerSliders">
-        <input type="range" min={0} max={1} step={0.01} value={volume} onChange={handleVolume} />
+      <FontAwesomeIcon icon={faVolumeHigh} /> <input type="range" min={0} max={1} step={0.01} value={volume} onChange={handleVolume} className="volumeBar" />
       </div>
       </div>
       <div className="playList">
         {songs.map((song, index) => (
           <div key={index} className="songOrder">
             <span>{song.title}</span>
-            <button onClick={() => playSong(index)} disabled={loading}>Play</button>
+            <button onClick={() => playSong(index)} disabled={loading} className="listButton">Play</button>
           </div>
         ))}
       </div>
